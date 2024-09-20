560463c92eacdbafb25aec6297c388b3f3bf104e
unknown <nwfakbart@gmail.com>
2024-09-21 01:08:33 +0700

    Initial commit

---
+++ app/src/main/java/com/nawaf/latihan_sep20/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.nawaf.latihan_sep20
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var gas : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        gas = findViewById(R.id.pindah)
+
+        gas.setOnClickListener(){
+            val intent = Intent(this, HomePage::class.java)
+            startActivity(intent)
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
